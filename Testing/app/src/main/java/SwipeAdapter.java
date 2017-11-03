6c7cfef214a89516aca750d0c186faf587bda82b
davidsip <davidsip@outlook.com>
2017-11-03 15:00:55 -0700

    add testing to alee

---
+++ Testing/app/src/main/java/SwipeAdapter.java
@@ -0,0 +1,32 @@
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by David on 11/3/2017.
+ */
+
+public class SwipeAdapter extends PagerAdapter {
+
+    private int[] time = {9,10,11,12,1,2};
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return false;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        return super.instantiateItem(container, position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        super.destroyItem(container, position, object);
+    }
+}
