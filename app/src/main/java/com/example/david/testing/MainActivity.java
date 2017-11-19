9c30ac9c24e05f97aee1a8d5794e58dbc6d0642c
Deepak Sharma <desharma@ucsc.edu>
2017-11-18 18:42:10 -0800

    working on getting JSON responses to work

--- app/src/main/java/com/example/david/testing/MainActivity.java
+++ app/src/main/java/com/example/david/testing/MainActivity.java
@@ -8,15 +8,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.dvdme.ForecastIOLib.FIOCurrently;
+import com.github.dvdme.ForecastIOLib.ForecastIO;
 import com.johnhiott.darkskyandroidlib.ForecastApi;
 import com.johnhiott.darkskyandroidlib.RequestBuilder;
 import com.johnhiott.darkskyandroidlib.models.Request;
 import com.johnhiott.darkskyandroidlib.models.WeatherResponse;
 
 
+import org.apache.http.HttpClientConnection;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestHandle;
+import com.loopj.android.http.RequestParams;
+
+
+import cz.msebera.android.httpclient.Header;
+import okhttp3.OkHttpClient;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,9 +46,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         double axx= weatherCurrent.ax;
         double ayy = weatherCurrent.ay;
+        String BASE_URL = "https://api.forecast.io/forecast/";
         super.onCreate(savedInstanceState);
-        ForecastApi.create("4fb2c715ea744173c72290437de1c776");
+        String apiKey = "4fb2c715ea744173c72290437de1c776";
+        ForecastApi.create(apiKey);
         setContentView(R.layout.activity_main);
+
 
         Button currentFood = (Button) findViewById(R.id.button13);
         Button currentBar = (Button) findViewById(R.id.button14);
@@ -42,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
         String Latitude = Double.toString(axx);
         String Longitude = Double.toString(ayy);
 
-
         Request request = new Request();
         request.setLat(Latitude);
         request.setLng(Longitude);
@@ -50,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
         request.setLanguage(Request.Language.ENGLISH);
         request.addExcludeBlock(Request.Block.CURRENTLY);
         request.removeExcludeBlock(Request.Block.CURRENTLY);
+
+        String url = "https://api.forecast.io/forecast";
+        AsyncHttpClient client = new AsyncHttpClient();
 
         weather.getWeather(request, new Callback<WeatherResponse>() {
             String TAG = null;
@@ -69,6 +94,8 @@ public class MainActivity extends AppCompatActivity {
                 weather_print.setText("failure to access weather");
             }
         });
+
+
 
         currentFood.setOnClickListener(new View.OnClickListener(){
             @Override
--- app/src/main/java/com/example/david/testing/weatherCurrent.java
+++ app/src/main/java/com/example/david/testing/weatherCurrent.java
@@ -8,13 +8,23 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.dvdme.ForecastIOLib.FIOCurrently;
+import com.github.dvdme.ForecastIOLib.ForecastIO;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.client.HttpClient;
+import okhttp3.OkHttpClient;
+import retrofit2.http.HTTP;
+
 
 /**
  * Created by Deepak Sharma on 11/17/2017.
@@ -35,11 +45,29 @@ class weatherCurrent extends AppCompatActivity implements
 
     public static double ax;
     public static double ay;
+    String Latitude = Double.toString(ax);
+    String Longitude = Double.toString(ay);
+    String apiKey = "4fb2c715ea744173c72290437de1c776";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ForecastIO fio = new ForecastIO(apiKey);
+        fio.setUnits(ForecastIO.UNITS_US);
+        fio.setLang(ForecastIO.LANG_ENGLISH);
+        fio.getForecast(Latitude, Longitude);
+        System.out.println("Latitude: " + fio.getLatitude());
+        System.out.println("Longitude: " + fio.getLongitude());
+        System.out.println("Timezone:" + fio.getTimezone());
+
+        FIOCurrently currently =new FIOCurrently(fio);
+        System.out.println("\nCurrently\n");
+        String [] f = currently.get().getFieldsArray();
+        for(int i = 0 ; i < f.length; i++) {
+            System.out.println(f[i]+": "+currently.get().getByKey(f[i]));
+        }
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 // The next two lines tell the new client that “this” current class will handle connection stuff
