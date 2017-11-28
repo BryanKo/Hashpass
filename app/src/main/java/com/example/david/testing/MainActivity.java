7f06765a476d246fd55009e11c809c11f49f1e9e
davidsip <davidsip@outlook.com>
2017-11-27 22:17:04 -0800

    new layout plus some pictures

--- app/src/main/java/com/example/david/testing/MainActivity.java
+++ app/src/main/java/com/example/david/testing/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.david.testing;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,11 +11,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -23,8 +25,8 @@ import java.util.Date;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-=======
->>>>>>> 996bf80849baf035cfab23da831a8e9b3c0e2b1d
+//=======
+//>>>>>>> 996bf80849baf035cfab23da831a8e9b3c0e2b1d
 import com.johnhiott.darkskyandroidlib.ForecastApi;
 import com.johnhiott.darkskyandroidlib.RequestBuilder;
 import com.johnhiott.darkskyandroidlib.models.Request;
@@ -37,66 +39,86 @@ import retrofit.client.Response;
 
 
 public class MainActivity extends AppCompatActivity {
-
-<<<<<<< HEAD
+    private RelativeLayout rLayout;
+//<<<<<<< HEAD
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView.Adapter mAdapter;
-    private ArrayList<String> mDataSet;
- //   private ArrayList<Button> mButtonSet;
-=======
->>>>>>> 996bf80849baf035cfab23da831a8e9b3c0e2b1d
+ //   private ArrayList<String> mDataSet;
+    private ArrayList<DataModel> allDataArray;
+//=======
+//>>>>>>> 996bf80849baf035cfab23da831a8e9b3c0e2b1d
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         double axx= weatherCurrent.ax;
         double ayy = weatherCurrent.ay;
         super.onCreate(savedInstanceState);
         ForecastApi.create("4fb2c715ea744173c72290437de1c776");
         setContentView(R.layout.activity_main);
-
+      //  rLayout.setBackground(ContextCompat.getDrawable(this,R.drawable.afternoon));
+        //Get Calendar and Date to display NOW and the next 5 hours;
         Calendar calendar = Calendar.getInstance();
         DateFormat dateFormat = new SimpleDateFormat("hh a");
         Date date = new Date();
+        int timeOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+        Drawable morn = (Drawable)getResources().getDrawable(R.drawable.amorning);
+        Drawable after = (Drawable)getResources().getDrawable(R.drawable.afternoon);
+        if(timeOfDay >= 0 && timeOfDay < 12){
+            Toast.makeText(this, "Good Morning", Toast.LENGTH_SHORT).show();
+        //    view.setBackground(morn);
+        }
+        else if(timeOfDay >= 12 && timeOfDay < 16){
+            Toast.makeText(this, "Good Afternoon", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(timeOfDay >= 16 && timeOfDay < 21){
+            Toast.makeText(this, "Good Evening", Toast.LENGTH_SHORT).show();
+        }
+        else if(timeOfDay >= 21 && timeOfDay < 24){
+            Toast.makeText(this, "Good Night", Toast.LENGTH_SHORT).show();
+        }
 
  //       Button currentFood = (Button) findViewById(R.id.foodButton);
         //Recycler view*************************************************************************8
-        mDataSet = new ArrayList<>();
-   //     mButtonSet = new ArrayList<>();
+        allDataArray = new ArrayList<>();
+       // mButtonSet = new ArrayList<>();
 
 //        for (int i = 0; i < 5; i++) {
 //            mDataSet.add (calendar.get(Calendar.HOUR+i)+""+calendar.get(Calendar.AM_PM));
 //        }
-        mDataSet.add ("NOW");
-        calendar.add(Calendar.HOUR, 1);
-        Date oneHour = calendar.getTime();
-        mDataSet.add (dateFormat.format(oneHour));
-        calendar.add(Calendar.HOUR, 1);
-        Date twoHour = calendar.getTime();
-        mDataSet.add (dateFormat.format(twoHour));
-        calendar.add(Calendar.HOUR, 1);
-        Date threeHour = calendar.getTime();
-        mDataSet.add (dateFormat.format(threeHour));
-        calendar.add(Calendar.HOUR, 1);
-        Date fourHour = calendar.getTime();
-        mDataSet.add (dateFormat.format(fourHour));
-        calendar.add(Calendar.HOUR, 1);
-        Date fiveHour = calendar.getTime();
-        mDataSet.add (dateFormat.format(fiveHour));
+//        allDataArray.add (new DataModel("Now", "Food"));
+//        calendar.add(Calendar.HOUR, 1);
+//        Date oneHour = calendar.getTime();
+//        allDataArray.add (new DataModel(dateFormat.format(oneHour), "Food2"));
+//        calendar.add(Calendar.HOUR, 1);
+//        Date twoHour = calendar.getTime();
+//        allDataArray.add ( new DataModel(dateFormat.format(twoHour), "Food3"));
+//        calendar.add(Calendar.HOUR, 1);
+//        Date threeHour = calendar.getTime();
+//        mDataSet.add (dateFormat.format(threeHour));
+//        calendar.add(Calendar.HOUR, 1);
+//        Date fourHour = calendar.getTime();
+//        mDataSet.add (dateFormat.format(fourHour));
+//        calendar.add(Calendar.HOUR, 1);
+//        Date fiveHour = calendar.getTime();
+//        mDataSet.add (dateFormat.format(fiveHour));
 //        int i = 0;
 //
 //        while (i<5){
 //            mDataSet.add (calendar.get(Calendar.HOUR)+""+calendar.get(Calendar.AM_PM));
 //            i++;
 //        }
-//        mButtonSet.add (currentFood);
 
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new MainAdapter(mDataSet);
+        //mAdapter = new MainAdapter(allDataArray);
+        mAdapter = new MainAdapter();
         mRecyclerView.setAdapter(mAdapter);
 
         //Recycler view*************************************************************************8
@@ -131,7 +153,8 @@ public class MainActivity extends AppCompatActivity {
                 double farenTemp = (celsiusTemp * (9/5)) + 32;
                 double tempSummary = Log.d(TAG, "Summary: " + weatherResponse.getCurrently().getSummary());
                 double tempHourly = Log.d(TAG, "Hourly Sum: " + weatherResponse.getHourly().getSummary());
-                weather_print.setText("Weather in Santa Cruz is " + (tempHourly) + " °F.");
+               // weather_print.setText("Weather in Santa Cruz is " + (tempHourly) + " °F.");
+                weather_print.setText((tempHourly) + " °F");
             }
 
             @Override
--- app/src/main/java/com/example/david/testing/MainAdapter.java
+++ app/src/main/java/com/example/david/testing/MainAdapter.java
@@ -1,5 +1,6 @@
 package com.example.david.testing;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,7 +8,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by David on 11/17/2017.
@@ -15,13 +20,35 @@ import java.util.ArrayList;
 
 class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder>{
 
-    private ArrayList<String> mDataSet;
-//    private ArrayList<Button> mButtonSet;
+    private ArrayList<DataModel> allDataArray;
+    //Get Calendar and Date to display NOW and the next 5 hours;
+    Calendar calendar = Calendar.getInstance();
+    DateFormat dateFormat = new SimpleDateFormat("hh a");
+    Date date = new Date();
+
+    public MainAdapter(){
+        allDataArray = new ArrayList<DataModel>(5);
+
+        allDataArray.add (new DataModel("Now", "Food"));
+        calendar.add(Calendar.HOUR, 1);
+        Date oneHour = calendar.getTime();
+        allDataArray.add (new DataModel(dateFormat.format(oneHour), "Food2"));
+        calendar.add(Calendar.HOUR, 1);
+        Date twoHour = calendar.getTime();
+        allDataArray.add ( new DataModel(dateFormat.format(twoHour), "Food3"));
+        calendar.add(Calendar.HOUR, 1);
+        Date threeHour = calendar.getTime();
+        allDataArray.add ( new DataModel(dateFormat.format(threeHour), "Food4"));
+    }
 
 
-    public MainAdapter(ArrayList<String> mDataSet){
-        this.mDataSet = mDataSet;
-//        this.mButtonSet = mButtonSet;
+    public Object getItem (int i){
+        return allDataArray.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
     }
 
     @Override
@@ -30,31 +57,34 @@ class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder>{
                 .inflate(R.layout.time, parent, false);
 
         ViewHolder viewHolder = new ViewHolder(view);
+
         return viewHolder;
     }
 
+
     @Override
     public void onBindViewHolder(MainAdapter.ViewHolder holder, int position) {
-        holder.mTime.setText(mDataSet.get(position));
-//        holder.mBut.setTag(mButtonSet.get(position));
+        DataModel dataModel = allDataArray.get(position);
+        holder.mTime.setText(dataModel.getTimeText());
+        holder.mBut.setText(dataModel.getButtonText());
     }
+
 
     @Override
     public int getItemCount() {
-        return mDataSet.size();
+        return allDataArray.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView mTime;
-  //      public Button mBut;
-
+        public Button mBut;
 
         public ViewHolder(View itemView) {
             super(itemView);
-
+            Context context = itemView.getContext();
             mTime = (TextView) itemView.findViewById(R.id.time_time);
- //           mBut = (Button) itemView.findViewById(R.id.button);
+            mBut = (Button) itemView.findViewById(R.id.button);
         }
     }
 }
