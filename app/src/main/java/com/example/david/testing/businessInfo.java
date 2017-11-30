ece54ca30d5326fc087f86011adfbb3df44e029f
Anthony Lee <ant.lee813@gmail.com>
2017-11-30 00:52:33 -0800

    BusinessProfile view works correctly
    ** fixed issue of image URL not being called correctly

--- app/src/main/java/com/example/david/testing/businessInfo.java
+++ app/src/main/java/com/example/david/testing/businessInfo.java
@@ -1,19 +1,27 @@
 package com.example.david.testing;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 
 /**
  * Created by Bryan on 11/28/2017.
  */
 
 public class businessInfo extends AppCompatActivity {
+
+    private ArrayList<String> businessesImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,7 @@ public class businessInfo extends AppCompatActivity {
         Log.d("bundleinfo", currbusinessImg + " " + currBusinessName + " " + currBusinessLoc + " " + currBusinessPrice + " " + passBusinessRating + " " + passBusinessReviewCnt + String.valueOf(round((currBusinessDist / 1609.34), 2)).concat(" miles"));
 
         // Call pulled-API info into view
+        new dlBusinessImg((ImageView) findViewById(R.id.ivBusinessProfilePic)).execute(currbusinessImg);
         cBusinessName.setText(currBusinessName);
         cBusinessAddr.setText(currBusinessLoc);
         cBusinessPrice.setText(currBusinessPrice);
@@ -49,7 +58,30 @@ public class businessInfo extends AppCompatActivity {
         cBusinessDist.setText(String.valueOf(round((currBusinessDist / 1609.34), 2)).concat(" mi"));
     }
 
+    private class dlBusinessImg extends AsyncTask<String, Void, Bitmap> {
+        ImageView businessImg;
 
+        public dlBusinessImg(ImageView businessImg) {
+            this.businessImg = businessImg;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String imgurl = urls[0];
+            Bitmap dledBusinessImg = null;
+            try {
+                InputStream inputStream = new java.net.URL(imgurl).openStream();
+                dledBusinessImg = BitmapFactory.decodeStream(inputStream);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return dledBusinessImg;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            businessImg.setImageBitmap(result);
+        }
+    }
 
     public static double round(double value, int places) {
         if (places < 0) throw new IllegalArgumentException();
